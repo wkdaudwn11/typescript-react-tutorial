dbf35e4f26a00a23773b842e8d76da4c44f4ae46
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-05-03 21:21:20 +0900

    인풋 상태 관리하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Greetings from "./Greetings";
 import Counter from "./Counter";
+import MyForm from "./MyForm";
 
 function App() {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
     <div>
       <Greetings name="MJ" onClick={onClick} />
       <Counter />
+      <MyForm onSubmit={onSubmit} />
     </div>
   );
 }
